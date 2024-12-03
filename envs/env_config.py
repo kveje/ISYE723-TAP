035b901b37c9aac6a4b87f1142aaff1924322b2c
kveje <kasper@vejejakobsen.dk>
2024-12-02 18:07:08 -0600

    updated env config

--- envs/env_config.py
+++ envs/env_config.py
@@ -8,7 +8,7 @@ class EnvConfig:
     number_teams: int = 10
     max_team_size: int = ceil(num_individuals / number_teams)
     num_periods: int = 1000
-    phi: float = 0.5
-    sigma_eta: float = 0.1
-    sigma_feedback: float = 0.1
+    sigma_w: float = 0.1
+    sigma_f: float = 0.1
+    sigma_p: float = 1
     metric: str = "L1"  # L1, L2 or Linf
