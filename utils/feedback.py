9cfd5ec265ca61533203aac41bd32acceefd48fb
kveje <kasper@vejejakobsen.dk>
2024-12-02 18:06:45 -0600

    deleted old framework

--- utils/feedback.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-def generate_feedback(true_preference, sigma_feedback):
-    noise = random.gauss(0, sigma_feedback)
-    feedback = max(0, min(1, true_preference + noise))
-    return feedback
--- utils/preference_evolution.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-def evolve_preference(current_preference, phi, sigma_eta):
-    eta = random.gauss(0, sigma_eta)
-    new_preference = max(0, min(1, phi * current_preference + eta))
-    return new_preference
