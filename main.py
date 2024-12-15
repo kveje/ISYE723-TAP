e15be40337acfe3014300fa0d75db973fc7b76e9
kveje <kasper@vejejakobsen.dk>
2024-12-15 18:00:30 +0100

    updated simulation framework

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from envs.team_assignment_env import TeamAssignmentEnv
 from agents.actors.random_actor import RandomActor
-from agents.actors.optimization_actor import OptimizationActor
+from agents.actors.ucb_actor import UCBActor
+from agents.actors.thompson_actor import ThompsonActor
 from agents.agent import Agent
 from agents.learners.kalman_learner import KalmanLearner
 from envs.env_config import EnvConfig
-import matplotlib.pyplot as plt
-from statistics import mean, stdev
 from utils.metrics import estimate_reward, calculate_distance
 from utils.visualization import aggregate, plot
 import numpy as np
+import os
+import json
 
 
 def test(env_config: EnvConfig, actor: object, learner: object):
@@ -21,22 +22,13 @@ def test(env_config: EnvConfig, actor: object, learner: object):
     # Initialize variables
     total_reward = 0
     done = False
-    rewards = []
-    estimated_rewards = []
+    rewards = np.zeros(env_config.num_periods)
+    estimated_rewards = np.zeros(env_config.num_periods)
     period = 0
-    preference_distances = []
+    preference_distances = np.zeros(env_config.num_periods)
 
     # Reset the environment
     observation = env.reset()
-
-    random_person_1 = np.random.randint(0, env_config.num_individuals)
-    random_person_2 = np.random.randint(0, env_config.num_individuals)
-    while random_person_1 == random_person_2:
-        random_person_2 = np.random.randint(0, env_config.num_individuals)
-
-    true_preferences = []
-    estimated_preferences = []
-    estimated_preferences_variances = []
 
     while not done:
         # Decide on an action
@@ -50,91 +42,89 @@ def test(env_config: EnvConfig, actor: object, learner: object):
 
         # Update the total reward
         total_reward += reward
-        rewards.append(reward)
+        rewards[period] = reward
 
         # Estimate the reward
-        estimated_rewards.append(estimate_reward(agent.get_beliefs()[0], action))
+        estimated_rewards[period] = estimate_reward(agent.get_beliefs()[0], action)
 
         # Calculate the distance between true preferences and estimated preferences
-        preference_distances.append(
-            calculate_distance(
-                env.individuals.get_true_preferences(),
-                agent.learner.get_means(),
-            )
+        preference_distances[period] = calculate_distance(
+            env.individuals.get_true_preferences(),
+            agent.learner.get_means(),
         )
 
         # Update period
         period += 1
 
-        # Save preferences for visualization
-        true_preferences.append(
-            env.individuals.get_true_preferences()[random_person_1, random_person_2]
-        )
-        estimated_preferences.append(
-            agent.learner.get_means()[random_person_1, random_person_2]
-        )
-        estimated_preferences_variances.append(
-            agent.learner.get_variances()[random_person_1, random_person_2]
-        )
-
         if period % 20 == 0:
             print(f"  Period {period} / {env_config.num_periods}")
 
-    # Plot true and estimated preferences
-    # plt.figure(figsize=(12, 6))
-    # plt.plot(true_preferences, label="True Preferences")
-    # plt.plot(estimated_preferences, label="Estimated Preferences")
-    # plt.fill_between(
-    #     range(len(estimated_preferences)),
-    #     [
-    #         estimated_preferences[i] - estimated_preferences_variances[i]
-    #         for i in range(len(estimated_preferences))
-    #     ],
-    #     [
-    #         estimated_preferences[i] + estimated_preferences_variances[i]
-    #         for i in range(len(estimated_preferences))
-    #     ],
-    #     alpha=0.2,
-    # )
-    # plt.xlabel("Period")
-    # plt.ylabel("Preference")
-    # plt.title("True vs Estimated Preferences")
-    # plt.legend()
-    # plt.show()
-
     return (
-        agent,
-        np.array(rewards),
-        np.array(estimated_rewards),
-        np.array(preference_distances),
-        np.array(true_preferences) - np.array(estimated_preferences),
+        rewards,
+        estimated_rewards,
+        preference_distances,
     )
 
 
 if __name__ == "__main__":
     NUM_SIMULATIONS = 10
-    BETAS = [0, 0.1, 0.5, 1, 2, 5]
+    ACTORS = [
+        "UCB_0",
+        "UCB_0.1",
+        "UCB_0.5",
+        "UCB_1",
+        "UCB_2",
+        "UCB_5",
+        "Random",
+        "Thompson",
+    ]
+    LEARNERS = ["Kalman"]
     NUM_INDIVIDUALS = 10
-    NUM_TEAMS = 3
-    MAX_TEAM_SIZE = 4
+    NUM_TEAMS = 4
+    MAX_TEAM_SIZE = 3
     NUM_PERIODS = 100
-    SIGMA_F = 0.1
-    SIGMA_W = 0.1
+    SIGMA_F = 0.5
+    SIGMA_W = 0.5
     SIGMA_P = 1
+    RANDOM_SUBSTITUTION = 0.1
 
-    # Initialize dictionaries to store rewards
-    rewards = {beta: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for beta in BETAS}
-    estimated_rewards = {
-        beta: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for beta in BETAS
-    }
-    preference_distances = {
-        beta: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for beta in BETAS
-    }
-    prefs = {beta: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for beta in BETAS}
+    # Create folders for storing results
+    if not os.path.exists("results"):
+        os.makedirs("results")
 
-    # Beta simulation
-    for beta in BETAS:
-        print(f"Starting beta: {beta}")
+    for actor_type in ACTORS:
+        if not os.path.exists(f"results/{actor_type}"):
+            os.makedirs(f"results/{actor_type}")
+
+    # Save Simulation Config
+    config = {
+        "NUM_SIMULATIONS": NUM_SIMULATIONS,
+        "ACTORS": ACTORS,
+        "LEARNERS": LEARNERS,
+        "NUM_INDIVIDUALS": NUM_INDIVIDUALS,
+        "NUM_TEAMS": NUM_TEAMS,
+        "MAX_TEAM_SIZE": MAX_TEAM_SIZE,
+        "NUM_PERIODS": NUM_PERIODS,
+        "SIGMA_F": SIGMA_F,
+        "SIGMA_W": SIGMA_W,
+        "SIGMA_P": SIGMA_P,
+    }
+
+    # Save the config as json
+    json.dump(config, open("results/config.json", "w"))
+
+    # Simulations
+    for actor_type in ACTORS:
+        actual_rewards = np.zeros(
+            shape=(NUM_SIMULATIONS, NUM_PERIODS), dtype=np.float32
+        )
+        estimated_rewards = np.zeros(
+            shape=(NUM_SIMULATIONS, NUM_PERIODS), dtype=np.float32
+        )
+        preference_distances = np.zeros(
+            shape=(NUM_SIMULATIONS, NUM_PERIODS), dtype=np.float32
+        )
+        print(f"Starting simlatuion with actor: {actor_type} and learner: Kalman")
         for i in range(NUM_SIMULATIONS):
             # Config
             env_config = EnvConfig(
@@ -148,87 +138,31 @@ if __name__ == "__main__":
             )
 
             # Create the agent
-            actor = OptimizationActor(env_config=env_config, beta=beta)
+            if actor_type.lower()[0:3] == "ucb":
+                beta = float(actor_type.split("_")[1])
+                actor = UCBActor(env_config=env_config, beta=beta)
+            elif actor_type.lower() == "random":
+                actor = RandomActor(env_config=env_config)
+            elif actor_type.lower() == "thompson":
+                actor = ThompsonActor(env_config=env_config)
+            else:
+                raise ValueError("Invalid actor type")
+
             learner = KalmanLearner(env_config=env_config)
 
             # Test the agent
-            agent, act_rewards, est_rewards, pref_dist, pref = test(
-                env_config, actor, learner
-            )
+            act_rewards, est_rewards, pref_dist = test(env_config, actor, learner)
 
             # Store the results
-            rewards[beta][i, :] = act_rewards
-            estimated_rewards[beta][i, :] = est_rewards
-            preference_distances[beta][i, :] = pref_dist
-            prefs[beta][i, :] = pref
+            actual_rewards[i, :] = act_rewards
+            estimated_rewards[i, :] = est_rewards
+            preference_distances[i, :] = pref_dist
 
-            print(f" Beta: {beta}, Simulation {i+1} / {NUM_SIMULATIONS} completed")
+            print(
+                f" Agent: {actor_type}, Simulation {i+1} / {NUM_SIMULATIONS} completed"
+            )
 
-    # Random simulation
-    # Initialize dictionaries to store rewards
-    rewards["Random"] = np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS))
-    estimated_rewards["Random"] = np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS))
-    preference_distances["Random"] = np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS))
-    prefs["Random"] = np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS))
-
-    print(f"Starting Random")
-    # Run the simulation
-    for _ in range(NUM_SIMULATIONS):
-        # Config
-        env_config = EnvConfig(
-            num_individuals=NUM_INDIVIDUALS,
-            number_teams=NUM_TEAMS,
-            max_team_size=MAX_TEAM_SIZE,
-            num_periods=NUM_PERIODS,
-        )
-
-        # Create the agent
-        actor = RandomActor(env_config=env_config)
-        learner = KalmanLearner(env_config=env_config)
-
-        # Test the agent
-        agent, act_rewards, est_rewards, pref_dist, pref = test(
-            env_config, actor, learner
-        )
-
-        # Store the results
-        rewards["Random"][i, :] = act_rewards
-        estimated_rewards["Random"][i, :] = est_rewards
-        preference_distances["Random"][i, :] = pref_dist
-        prefs["Random"][i, :] = pref
-
-        print(f" Random, Simulation {i+1} / {NUM_SIMULATIONS} completed")
-
-    # Aggregate results
-    mean_rewards = aggregate(rewards)
-    mean_estimated_rewards = aggregate(estimated_rewards)
-    mean_distances = aggregate(preference_distances)
-    mean_prefs = aggregate(prefs)
-
-    x_label = f"""Period
-               No. of Individuals: {NUM_INDIVIDUALS},   No. of Teams: {NUM_TEAMS},   Max Team Size: {MAX_TEAM_SIZE},   $\sigma_f=${SIGMA_F},   $\sigma_w=${SIGMA_W},   $\sigma_p=${SIGMA_P}"""
-    title = "Performance of Different Actors"
-
-    # Plot results
-    plot(mean_rewards, x_label, "Reward", title, "results/rewards.png")
-    plot(
-        mean_estimated_rewards,
-        x_label,
-        "Estimated Reward",
-        title,
-        "results/estimated_rewards.png",
-    )
-    plot(
-        mean_distances,
-        x_label,
-        "Preference Distance",
-        title,
-        "results/preference_distance.png",
-    )
-    plot(
-        mean_prefs,
-        x_label,
-        "Preference Difference (two random individuals)",
-        title,
-        "results/preference_difference.png",
-    )
+        # Save the results
+        np.save(f"results/{actor_type}/actual_rewards.npy", actual_rewards)
+        np.save(f"results/{actor_type}/estimated_rewards.npy", estimated_rewards)
+        np.save(f"results/{actor_type}/preference_distances.npy", preference_distances)
