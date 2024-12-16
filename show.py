a452fe2b378a1f53c14616566830b45942bed2a4
kveje <kasper@vejejakobsen.dk>
2024-12-16 10:02:36 +0100

    minor

--- show.py
+++ show.py
@@ -1,4 +1,4 @@
-PATH = "results1"
+PATH = "results_2"
 import numpy as np
 import matplotlib.pyplot as plt
 from utils.metrics import estimate_reward, calculate_distance
@@ -18,6 +18,7 @@ SIGMA_W = config["SIGMA_W"]
 SIGMA_P = config["SIGMA_P"]
 NUM_SIMULATIONS = config["NUM_SIMULATIONS"]
 NUM_PERIODS = config["NUM_PERIODS"]
+RANDOM_SUBSTITUTION = config.get("RANDOM_SUBSTITUTION", 0)
 
 # Initialize variables
 actual_rewards = {
@@ -27,6 +28,9 @@ estimated_rewards = {
     actor_name: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for actor_name in ACTORS
 }
 preference_distances = {
+    actor_name: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for actor_name in ACTORS
+}
+times = {
     actor_name: np.zeros(shape=(NUM_SIMULATIONS, NUM_PERIODS)) for actor_name in ACTORS
 }
 
@@ -40,18 +44,21 @@ for actor_name in ACTORS:
     preference_distances[actor_name] = np.load(
         f"{PATH}/{actor_name}/preference_distances.npy"
     )
+    times[actor_name] = np.load(f"{PATH}/{actor_name}/times.npy")
 
 # Aggregate results
 agg_actual_rewards = aggregate(actual_rewards)
 agg_estimated_rewards = aggregate(estimated_rewards)
 agg_distances = aggregate(preference_distances)
+agg_times = aggregate(times)
 cum_agg_actual_rewards = cummulative_aggregate(actual_rewards)
 cum_agg_estimated_rewards = cummulative_aggregate(estimated_rewards)
+cum_agg_times = cummulative_aggregate(times)
 
 # Prepare labels
 x_label = f"""Period
-    #Individuals: {NUM_INDIVIDUALS},  #Teams: {NUM_TEAMS},  $alpha=${MAX_TEAM_SIZE},  $\sigma_f=${SIGMA_F},  $\sigma_w=${SIGMA_W},  $\sigma_p=${SIGMA_P}"""
-title = "Performance of Different Actors"
+#Individuals: {NUM_INDIVIDUALS},  #Teams: {NUM_TEAMS},  $alpha=${MAX_TEAM_SIZE},  $\sigma_f=${SIGMA_F},  $\sigma_w=${SIGMA_W},  $\sigma_p=${SIGMA_P}, $\gamma=${RANDOM_SUBSTITUTION}"""
+title = "of Different Actors"
 color_map = create_color_map(ACTORS)
 
 # Plot results
@@ -59,7 +66,7 @@ plot(
     agg_actual_rewards,
     x_label,
     "Reward",
-    title,
+    f"Reward {title}",
     f"{PATH}/rewards.png",
     color_map,
 )
@@ -67,7 +74,7 @@ plot(
     agg_estimated_rewards,
     x_label,
     "Estimated Reward",
-    title,
+    f"Estimated Reward {title}",
     f"{PATH}/estimated_rewards.png",
     color_map,
 )
@@ -75,7 +82,7 @@ plot(
     agg_distances,
     x_label,
     "Preference Distance",
-    title,
+    f"Preference Distance {title}",
     f"{PATH}/preference_distance.png",
     color_map,
 )
@@ -85,7 +92,7 @@ plot(
     cum_agg_actual_rewards,
     x_label,
     "Cumulative Reward",
-    title,
+    f"Cumulative Reward {title}",
     f"{PATH}/cumulative_rewards.png",
     color_map,
 )
@@ -94,7 +101,26 @@ plot(
     cum_agg_estimated_rewards,
     x_label,
     "Cumulative Estimated Reward",
-    title,
+    f"Cumulative Estimated {title}",
     f"{PATH}/cumulative_estimated_rewards.png",
+    color_map,
+)
+
+# Plot time results
+plot(
+    agg_times,
+    x_label,
+    "Time (s)",
+    f"Time {title}",
+    f"{PATH}/times.png",
+    color_map,
+)
+
+plot(
+    cum_agg_times,
+    x_label,
+    "Cumulative Time (s)",
+    f"Cumulative Time {title}",
+    f"{PATH}/cumulative_times.png",
     color_map,
 )
