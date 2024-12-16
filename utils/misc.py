100a0f4c3ae918006511c39f2bd0a7b9e949eb9a
kveje <kasper@vejejakobsen.dk>
2024-12-16 13:08:33 +0100

    minor

--- utils/misc.py
+++ utils/misc.py
@@ -1,9 +1,12 @@
-def show_name(actor_type: str):
-    if actor_type.lower()[:3] == "ucb":
-        return "UCB: " + r"$\beta =$" + f" {actor_type[4:]}"
-    elif actor_type.lower() == "random":
-        return "Random"
-    elif actor_type.lower() == "thompson":
-        return "Thompson"
-    else:
-        return actor_type
+def show_name(actor_type):
+    if type(actor_type) == str:
+        if actor_type.lower()[:3] == "ucb":
+            return "UCB: " + r"$\beta =$" + f" {actor_type[4:]}"
+        elif actor_type.lower() == "random":
+            return "Random"
+        elif actor_type.lower() == "thompson":
+            return "Thompson"
+        else:
+            return actor_type
+    elif type(actor_type) == int:
+        return r"$\gamma =$" + f" {actor_type}"
